4858ad47f99f827daa8311c4d7f7149493a246aa
Sukumarsawant <your_email@example.com>
2025-07-24 20:08:23 +0530

    exp 1

--- insertion.c
+++ insertion.c
@@ -2,12 +2,12 @@
 #include<conio.h>
 #include<math.h>
 #include<time.h>
-
+#include<stdlib.h>
 void insertion(int a[],int n){
 	int i , j ,val ; 
 	for(i =1 ; i<n;++i){
 		val = a[i];
-		for( j = i+1;j>=0;--j){
+		for( j = i-1;j>=0;--j){
 			if(val<a[j]){
 				a[j+1] = a[j];
 			}
@@ -18,74 +18,60 @@ void insertion(int a[],int n){
 }
 void selection(int a[],int n){
 	int i , j , mini,idx ;
-	mini = a[0];
-	idx = 0;
-	for(i=1 ;i<n-1;++i){
-		for( j = 0 ; j<n;++j){
-			if(mini<a[j]){
+	
+	for(i=0 ;i<n-1;++i){
+		mini = a[i];
+	idx = i;
+		for( j =i+1  ; j<n;++j){
+			if(mini>a[j]){
 			mini = a[j];
 			idx  = j;
 		}
+		}
 		a[idx] = a[i];
 		a[i] = mini;
-		
-		}
 	}
 }
 int main(){
-	int i , j ,temp; 
-	int n = 10000;
+	int n =10;
 	int a[n],b[n];
-	clock_t st ,ed ;
-	FILE* fptr1,*fptr2,*fptr3;
+	int temp;
+	int i , j ;
+	clock_t start,end;
+	double t;
+	FILE *fptr1,*fptr2;
 	fptr1 = fopen("input.txt","w");
-//	if(fptr1==NULL)printf("File not opened");
-	
-	for(i =0 ; i<n;++i){
-		fprintf(fptr1,"%d ",rand());
+
+	for( i =0 ; i<n;++i){
+		fprintf(fptr1,"%d ",rand());	
 	}
 	fclose(fptr1);
 		fptr1 = fopen("input.txt","r");
-	
-//	fscanf(fptr1,"%d",c);
-//	fptr2 = fopen("text.txt","w");
-//	for(i =0 ;i<n;++i){
-//		
-//		fprintf(fptr1,"%d ",c[i]);
-//		
-//	}
-//	rewind(fptr1);
-//	fclose(fptr2);
-//fptr2 = fopen("output1.txt")
-	for(i =0;i<n;++i){
-		  fscanf(fptr1,"%d",&temp);
-//		  printf("%d ",temp);
-		a[i] = temp ;
-		b[i] = temp ;
+	for(i =0 ; i<n;++i){
+		fscanf(fptr1,"%d",&temp);
+		a[i] = temp;
+		b[i] = temp;
 	}
-//	printf("okkk");
-	st =clock();
-		insertion(a,n);
-	ed = clock();
-//	printf("%f\n",ed);
-	printf("\n%lf\n",abs(ed-st)/1000.);
-	st = clock();
-		selection(b,n);
-		ed =clock();
-		printf("\n%lf\n",abs(ed-st)/1000.);
-//		printf("ok\n");
-		fclose(fptr1);
-			fptr2 = fopen("output1.txt","w");
-					fptr3 = fopen("output2.txt","w");
-//					for( i =0 ; i<n;++i)printf("%d ",a[i]);
-		for(i =0 ; i<n;++i){
-			fprintf(fptr2,"%d ",a[i]);
-			fprintf(fptr3,"%d ",b[i]);
-		}
-		fclose(fptr2);
-		fclose(fptr3);
-		printf("sorted");
-		
+	fclose(fptr1);
+	start = clock();
+	insertion(a,n);
+	end = clock();
+	t = (double)(end-start)/CLOCKS_PER_SEC;
+	start = clock();
+	selection(b,n);
+	end = clock();
+	printf("selection sort: %lf\ninsertion sort: %lf\n",(double)(end-start)/CLOCKS_PER_SEC,t);
+
+	fptr1 = fopen("output1.txt","w");
+	fptr2 = fopen("output2.txt","w");
+	for(int i =0 ; i<n;++i){
+		fprintf(fptr1,"%d ",a[i]);
+		fprintf(fptr2,"%d ",b[i]);
+	}
+	fclose(fptr1);
+	fclose(fptr2);
+
+	
 	
 	
 	
