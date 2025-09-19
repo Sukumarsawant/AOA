3a8e86db68c4a9d47c24440570ca2fb9d4501031
Sukumarsawant <sawantsukumar@gmail.com>
2025-09-19 11:26:05 +0530

    exp9 0/1knasack

--- exp9_01Knapsack.c
+++ exp9_01Knapsack.c
@@ -3,31 +3,31 @@
 #include<math.h>
 #include<stdlib.h>
 
-#define MAX 10000
+#define MAX 100
 int max(int a,  int b){
     if(a>b) return a;
     else return b;
 }
-int add =0 ;
+// int
+int dp[MAX][MAX];
 int func(int i,int p[],int w[],int capacity){
     if(i<0) return 0;
-    
-    if(capacity<w[i]) return 0;
-    
-    
-    if(capacity>w[i]){
-        printf("%d\t",capacity-w[i]);
-     add = max(func(i-1,p,w,capacity-w[i])+p[i],func(i-1,p,w,capacity));
-    }
-     
-    return add;
+
+    if(capacity>=0 && dp[i][capacity]!=-1) return dp[i][capacity];
+    if(capacity>=w[i]) return  dp[i][capacity]=max(func(i-1,p,w,capacity-w[i])+p[i],func(i-1,p,w,capacity));
+    else return dp[i][capacity]=func(i-1,p,w,capacity);
+ 
 
 }
 int main(){
     int n, m ;
     int p[MAX],w[MAX]; 
     scanf("%d %d",&n,&m);
-
+    for(int i =0 ;i<=n;++i){
+        for(int j =0 ; j<=m;++j){
+            dp[i][j] = -1;
+        }
+    }
 
     for(int i =0 ;i<n;++i){
         scanf("%d",&p[i]);
@@ -36,7 +36,7 @@ int main(){
     for(int i =0 ;i<n;++i){
         scanf("%d",&w[i]);
     }
-
+    
     printf("%d",func(n-1,p,w,m));
 
 
